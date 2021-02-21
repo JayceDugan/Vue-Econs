c2a194e8d658cb1b3af129f83cac73fc41c7a996
Jayce Dugan <jayced@aspedia.net>
2021-02-21 08:20:00 +0000

    feat: Types added.

---
+++ src/types/MapObject.ts
@@ -0,0 +1,8 @@
+interface ListObject {
+  [key: string]: string
+}
+
+export interface MapObjectInterface {
+  list: ListObject,
+  keys: Array<string>,
+}
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export { MapObjectInterface } from './MapObject';
