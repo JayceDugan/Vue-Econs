0ccc870cf9f0f591f25163052da8785f0aa5b56f
Jayce Dugan <jayced@aspedia.net>
2021-02-21 08:18:10 +0000

    chore: Removed basic app scaffolding and rendered econ component.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <section class="app">
+    <econ icon="ec-address" />
+  </section>
 </template>
 
 <script lang="ts">
+import { defineAsyncComponent } from 'vue';
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    econ: defineAsyncComponent(() => import('./components/Econ.vue')),
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
 </style>
